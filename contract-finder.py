1c39016084af12b82869bbd9d09eda1397a51f71
SirLord <andrewacropora@gmail.com>
2020-02-22 01:03:09 -0500

    Figuring out IDE

---
+++ contract-finder.py
@@ -0,0 +1,30 @@
+"""
+Inert description
+
+@author=SirLord 
+"""
+
+import sys
+from datetime import datetime, date
+import logging
+import csv
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
+#logging.disable(logging.DEBUG)  #Disable Debug Messages
+
+#-Add scripts to fetch Data
+logging.debug('Start of program')
+#exampleFile = open('example.csv')
+#exampleReader = csv.reader(exampleFile)
+
+
+
+#-Munge Data and load into database
+#-Initial Flask App to display data
+#-Add queries and filters
+#-Viz Engine to display geographics
+#-Recommender Engine 
+#-Setup Instructions
+#-unit tests and coverage
+#-pylint to prettify
+logging.debug('End of program')
